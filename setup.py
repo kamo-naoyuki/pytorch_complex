935a737e779a5ddc029394d47d0bc7bfa55ae04b
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2020-05-02 17:11:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-dirname = os.path.dirname(__file__)↲
+dirname = os.path.dirname(__file__)
 setup(name='torch_complex',
       version='0.0.3',
       description='A fugacious python class for PyTorch-ComplexTensor',
