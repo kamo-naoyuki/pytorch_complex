babf317e132f82caf9e3e635b220f99906069b38
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2020-07-16 20:52:53 +0900

    Update test_basic_math.py

--- tests/test_basic_math.py
+++ tests/test_basic_math.py
@@ -97,12 +97,12 @@ def test_mean():
 def test_inverse():
     t = ComplexTensor(_get_complex_array(1, 10, 10))
     x = t @ t.inverse()
-    numpy.testing.assert_allclose(x.real.numpy()[0], numpy.eye(10), atol=1e-13)
-    numpy.testing.assert_allclose(x.imag.numpy()[0], numpy.zeros((10, 10)), atol=1e-13)
+    numpy.testing.assert_allclose(x.real.numpy()[0], numpy.eye(10), atol=1e-12)
+    numpy.testing.assert_allclose(x.imag.numpy()[0], numpy.zeros((10, 10)), atol=1e-12)
 
 
 def test_inverse2():
     t = ComplexTensor(_get_complex_array(1, 10, 10))
     x = t @ t.inverse2()
-    numpy.testing.assert_allclose(x.real.numpy()[0], numpy.eye(10), atol=1e-13)
-    numpy.testing.assert_allclose(x.imag.numpy()[0], numpy.zeros((10, 10)), atol=1e-13)
+    numpy.testing.assert_allclose(x.real.numpy()[0], numpy.eye(10), atol=1e-12)
+    numpy.testing.assert_allclose(x.imag.numpy()[0], numpy.zeros((10, 10)), atol=1e-12)
